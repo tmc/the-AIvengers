f2947f3a23738e5aea939c2a0fe873c0c2a43520
Travis Cline <travis.cline@gmail.com>
2023-07-22 17:52:12 -0700

    api: basic wiring

---
+++ fastapi-backend/aivengers.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class Agent:
+    name: str
+    description: str
--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -1,6 +1,6 @@
 # hackathon templates - fastapi backend - main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 # dotenv:
 from dotenv import load_dotenv
@@ -8,6 +8,33 @@ load_dotenv()
 
 app = FastAPI()
 
-@app.get("/webhooks/linear")
-async def root():
-    return {"message": "Hello World"}
+from aivengers import Agent
+
+AGENTS = {
+    "oracle": Agent(name="pm", description="Product Manager"),
+    "pm": Agent(name="pm", description="Product Manager"),
+    "architect": Agent(name="architect", description="Architect"),
+    "swe": Agent(name="swe", description="Software Engineer"),
+}
+
+@app.post("/webhooks/linear")
+async def webhooks_linear(request: Request):
+    j = await request.json()
+    webhook_result = handle_incoming_webhook(j)
+
+    print(webhook_result)
+    return {"status": "ok"}
+
+
+
+
+
+
+
+
+async def handle_incoming_webhook(payload: dict):
+    """
+    Handle incoming webhook from Linear
+    """
+    print("handle_incoming_webhook")
+
