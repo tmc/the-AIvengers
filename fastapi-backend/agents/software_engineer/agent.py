f2589abb297b1100b5c9df2ef70c23b9ff52df30
Tony A. Liu <67339555+tonyadastra@users.noreply.github.com>
2023-07-25 14:44:41 -0700

    clean up

--- fastapi-backend/agents/software_engineer/agent.py
+++ fastapi-backend/agents/software_engineer/agent.py
@@ -5,18 +5,15 @@ import os
 
 class SWEAgent(Agent):
     def __init__(self):
-        self.name = "Software Engineer"
+        self.name = "swe"
         self.description = "Software Engineer"
         self.linear_id = os.getenv("LINEAR_ID_SWE")
         os.environ['LINEAR_API_KEY'] = os.environ['LINEAR_API_KEY_SWE']
         
-    # def __init__(self, name, description, linear_id):
-    #     super().__init__(name, description, linear_id)
-    #     os.environ['LINEAR_API_KEY'] = os.environ['LINEAR_API_KEY_SWE']
         
-    async def __call__(self, issue_id: str, project_req: str, uml: str, folder_output: str):
+    async def __call__(self, issue_id: str, project_req: str, uml: str, folder_output: str, filename: str = 'App.js'):
         client = self.init_client()
         await self.start_up_comment(client=client, issue_id=issue_id)
         
-        code= write_code(input_prompt=project_req + "\n" + uml + "\nFolder structure: \n" + folder_output, out_file="App.js")
-        await self.comment(client=client,body="Generated code: \n```" + code+ '\n```', issue_id=issue_id)
+        code= write_code(project_reqs=project_req, uml=uml, structure=folder_output, out_file=filename)
+        await self.comment(client=client,body=f"Generated code: \n{filename}\n```\n" + code+ '\n```', issue_id=issue_id)
