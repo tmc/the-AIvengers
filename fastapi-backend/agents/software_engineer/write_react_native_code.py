61e1074e3e8bb38b9c317e83b06e537b31147a58
Tony A. Liu <67339555+tonyadastra@users.noreply.github.com>
2023-07-25 14:44:51 -0700

    add custom instructions for swe

--- fastapi-backend/agents/software_engineer/write_react_native_code.py
+++ fastapi-backend/agents/software_engineer/write_react_native_code.py
@@ -2,21 +2,21 @@ import argparse
 from langchain.chat_models import ChatOpenAI
 import re
 
-prompt = '''
-Simple visualization app using react native code:
-1. Visualize these images in row by row, max width is 500px: 
-- https://images.unsplash.com/photo-1682685796965-9814afcbff55
-- https://images.unsplash.com/photo-1661956601349-f61c959a8fd4
-2. Add title "AIvenger demo show"
-3. Write "Hello world" as body texts
-4. Add a button "Click me" at the bottom, when click, it will hide/unhide all images
 
-Constraints:
-- Please only use javascript syntax and react-native API, don't use any other libraries
-- only reply code, don't reply any other text
+def get_prompt(project_reqs: str, uml: str = None, structure: dict = None):
+    base_prompt = f'''
+    Generate code for the following project: 
+    {project_reqs}
+    {"UML: " + uml if uml else ""}
+    {"Structure: " + structure.__str__() if structure else ""}
 
-// Your javascript code here:
-'''
+    Constraints:
+    - Please only use javascript syntax and react-native API, don't use any other libraries
+    - only reply code, don't reply any other text
+
+    // Your javascript code here:
+    '''
+    return  base_prompt
 
 llm = ChatOpenAI(temperature=0.9, model_name='gpt-3.5-turbo-0613')
 
@@ -29,8 +29,9 @@ def extract_code(string):
     else:
         return string
 
-def write_code(input_prompt=prompt, out_file=None):
-    res = llm.predict(input_prompt)
+def write_code(project_reqs, uml, structure, out_file=None):
+    prompt = get_prompt(project_reqs, uml, structure)
+    res = llm.predict(prompt)
     extracted_code = extract_code(res)
 
     if out_file:
